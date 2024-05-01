2e4b09d0e31b7d50e8f9f70bf811986ca25d5915
Curt Commander <curtcommander@gmail.com>
2024-05-01 07:50:49 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [
+    "@typescript-eslint",
+  ],
+  rules: {
+    "indent": [ "error", 2 ],
+    "max-len": [ "error", {
+      "ignoreUrls": true,
+      "ignoreStrings": true,
+      "ignoreTemplateLiterals": true,
+      "ignoreRegExpLiterals": true,
+    } ],
+    "quotes": [ "error", "single" ],
+    "semi": [ "error", "always" ],
+    "array-bracket-spacing": [ "error", "always" ],
+    "object-curly-spacing": [ "error", "always" ],
+    "no-trailing-spaces": [ "error" ],
+    "no-multi-spaces": [ "error" ],
+    "no-multiple-empty-lines": [ "error", { "max": 1 } ],
+    "eol-last": [ "error", "always" ],
+    "@typescript-eslint/comma-dangle": [ "error", {
+      "arrays": "always-multiline",
+      "objects": "always-multiline",
+      "imports": "always-multiline",
+      "exports": "always-multiline",
+      "functions": "always-multiline",
+      "enums": "always-multiline",
+      "generics": "always-multiline",
+      "tuples": "always-multiline",
+    }],
+  },
+}
