3a335d2fa8de05f8c3fa2e2d8a0bf06ba1cec2f7
Curt Commander <curtcommander@gmail.com>
2024-05-03 10:39:16 -0500

    remove tsconfigRootDir from parserOptions, switch ts-eslint config to strict

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 module.exports = {
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/strict",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
     project: true,
-    tsconfigRootDir: __dirname,
   },
   plugins: [
     "@typescript-eslint",
